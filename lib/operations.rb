91477307099355e9e27ad49e061eddac34bf29e2
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-10-11 19:02:49 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,12 @@
 require 'pry'
 def unsafe?(speed)
-if speed > 60
-  return true
-elsif speed < 40
-  return true
-end
+    if speed > 60
+    return true
+      elsif speed < 40
+      return true
+        elsif 40 < speed < 60
+        return false
+  end
 end
 
 
